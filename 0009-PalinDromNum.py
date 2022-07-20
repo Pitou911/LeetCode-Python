a3304b1ee7ad8abc057310eb5412137720ebf5f8
Song Pitou <songpitou723@gmail.com>
2022-07-20 21:29:52 +0700

    add second method to palindrome

--- 0009-PalinDromNum.py
+++ 0009-PalinDromNum.py
@@ -1,15 +1,33 @@
-def isPalindrome(x):
-        """
-        :type x: int
-        :rtype: bool
-        """
-        string = str(x)
-        for i in range(len(string)):
-            if string[i] != string[len(string)-i-1]:
-                return False
-        return True
+#convert int to string
+# def isPalindrome(x):
+#         """
+#         :type x: int
+#         :rtype: bool
+#         """
+#         string = str(x)
+#         for i in range(len(string)):
+#             if string[i] != string[len(string)-i-1]:
+#                 return False
+#         return True
 
-if isPalindrome(1213):
+#second method:
+def isPalindrome(x):
+    if x < 0:
+        return False
+    div = 1
+    while x >= div * 10:
+        div = div * 10
+    while x:
+        left = x % 10
+        right = x // div
+        if left != right:
+            return False
+        else:
+            x = (x % div)//10
+            div /= 100
+    return True
+
+if isPalindrome(1213121):
     print("It is Palindrome Number")
 else:
     print("It is not Palindrome Number")
