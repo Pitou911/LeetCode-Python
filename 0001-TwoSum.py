0f1405753ae06fb4e6e52ee39a35bf40bb16103f
Song Pitou <songpitou723@gmail.com>
2022-07-20 21:18:05 +0700

    add other method to two sum

--- 0001-TwoSum.py
+++ 0001-TwoSum.py
@@ -1,13 +1,28 @@
+# using bruteForce O(n^2)
+# def twoSum(nums, target):
+#         """
+#         :type nums: List[int]
+#         :type target: int
+#         :rtype: List[int]
+#         """
+#         for i in range(len(nums)-1):
+#             for j in range(i+1, len(nums)):
+#                 if nums[i] + nums[j] == target:
+#                     return [i,j]
+#         return []
+
+# using hashmap aka Dictionary O(n)
 def twoSum(nums, target):
-        """
-        :type nums: List[int]
-        :type target: int
-        :rtype: List[int]
-        """
-        for i in range(len(nums)-1):
-            for j in range(i+1, len(nums)):
-                if nums[i] + nums[j] == target:
-                    return [i,j]
-        return []
+    lookUp = {}
+    res = []
+    for i in range(len(nums)):
+        complement = target - nums[i]
+        if lookUp.has_key(complement):
+            res.append(lookUp[complement])
+            res.append(i)
+            break
+        else:
+            lookUp[nums[i]] = i
+    return res
 
 print(twoSum([3,2,3], 6))
